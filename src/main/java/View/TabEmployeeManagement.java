01bda2278102b9eef026b8e16ebcd9c9ca95265d
Lilian Ghesquiere <lilian.ghesquiere@etu.univ-tours.fr>
2024-05-15 15:40:23 +0200

    Ajout package views séparation en plusiquers classe

---
+++ src/main/java/View/TabEmployeeManagement.java
@@ -0,0 +1,12 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+
+public class TabEmployeeManagement extends Tab {
+    public TableColumn TableView_employeeManagement;
+    public Button button_add;
+    public Button boutton_remove;
+    public Button bouton_modify;
+}
---
+++ src/main/java/View/TabTimeClocking.java
@@ -0,0 +1,17 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+
+public class TabTimeClocking extends Tab {
+
+    public Button bouton_history;
+    public ComboBox<String> CB_TimeClocking;
+    public Button bouton_Today;
+    public TableColumn TableViewTimeCloking;
+    public Button bouton_Checked;
+    public Button bouton_unchecked;
+
+}
