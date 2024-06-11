4f05781e2b4f64d9e086aa748b01df5f8563aaae
liliangsqr <lilian.gsqr@gmail.com>
2024-06-11 17:39:46 +0200

    • affichage HistoryOfAllTime

--- src/main/java/serialization/TimeClockingData.java
+++ src/main/java/serialization/TimeClockingData.java
@@ -11,14 +11,16 @@ import java.time.LocalTime;
 
 
 public class TimeClockingData {
+
     //private static ObservableList<TimeClocking> timeClockingList =  Deserializer.deserializePointages("SaveTimeClockings.ser");
     private static ObservableList<TimeClocking> timeClockingList = FXCollections.observableArrayList(
-            new TimeClocking(LocalDateTime.now(), new Employee(1,"michel","fromage","cadre",
-                    "michel.fromage@gmail.com","0625487962",12,new Planning(LocalTime.of(8, 0),
-                    LocalTime.of(17, 0))))
-            );
+            new TimeClocking(LocalDateTime.now(),EmployeeData.getEmployeeList().get(1)),
+            new TimeClocking(LocalDateTime.now(),EmployeeData.getEmployeeList().get(2))
+    );
 
-    public static ObservableList<TimeClocking> getTimeClockingList() { return timeClockingList; }
+
+    public static ObservableList<TimeClocking> getTimeClockingList() {
+        return timeClockingList; }
 
     public static void addTimeClocking(Employee employee) {
         TimeClocking timeClocking = new TimeClocking(LocalDateTime.now(), employee);
